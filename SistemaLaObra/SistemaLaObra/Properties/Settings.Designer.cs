98aa4adc5343d9182fc211ed34e6a94b9b2130bc
Ezequielem <56328685+Ezequielem@users.noreply.github.com>
2020-06-01 15:55:25 -0300

    Se actualiza

--- SistemaLaObra/SistemaLaObra/Properties/Settings.Designer.cs
+++ SistemaLaObra/SistemaLaObra/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SistemaLaObra.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace SistemaLaObra.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-BDPG71E\\SQLEXPRESS;Initial Catalog=LaObra;Integrated Security" +
-            "=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=PC-EZEQUIEL\\SQLEXPRESS;Initial Catalog=LaObra;Integrated Security=Tru" +
+            "e")]
         public string LaObraConnectionString {
             get {
                 return ((string)(this["LaObraConnectionString"]));
